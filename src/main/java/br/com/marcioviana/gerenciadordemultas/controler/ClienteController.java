30305943169fee64d57e1d528616bf6068967456
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2024-03-10 23:15:33 -0300

    feat: implementacao do metodo para cadastrar novo cliente através de metodo POST

--- src/main/java/br/com/marcioviana/gerenciadordemultas/controler/ClienteController.java
+++ src/main/java/br/com/marcioviana/gerenciadordemultas/controler/ClienteController.java
@@ -1,5 +1,6 @@
 package br.com.marcioviana.gerenciadordemultas.controler;
 
+import br.com.marcioviana.gerenciadordemultas.model.Cliente;
 import br.com.marcioviana.gerenciadordemultas.repository.ClienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/clientes")
@@ -21,6 +24,12 @@ public class ClienteController {
 
     @PostMapping
     public ResponseEntity<Void> cadastrar(@RequestBody @Valid ClienteRequest clienteRequest, UriComponentsBuilder uriComponentsBuilder){
+        Cliente novoCliente = clienteRequest.paraCliente();
 
+        clienteRepository.save(novoCliente);
+
+        URI caminho = uriComponentsBuilder.path("/clientes/{id}").buildAndExpand(novoCliente.getId()).toUri();
+
+        return ResponseEntity.created(caminho).build();
     }
 }
