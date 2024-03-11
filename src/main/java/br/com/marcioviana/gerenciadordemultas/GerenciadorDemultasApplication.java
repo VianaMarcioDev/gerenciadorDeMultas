fe672c33f3327e8255ee6a6f13468623504bcd89
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2024-03-10 22:14:16 -0300

    Initial commit

---
+++ src/main/java/br/com/marcioviana/gerenciadordemultas/GerenciadorDemultasApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.gerenciadordemultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDemultasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorDemultasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/marcioviana/gerenciadordemultas/GerenciadorDemultasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.gerenciadordemultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDemultasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
