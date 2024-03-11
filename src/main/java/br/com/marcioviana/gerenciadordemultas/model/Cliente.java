175ed73376fde163a2c6695e54f1e1d38470c736
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2024-03-10 22:33:25 -0300

    feat: criar entidade Cliente e seus atributos

---
+++ src/main/java/br/com/marcioviana/gerenciadordemultas/model/Cliente.java
@@ -0,0 +1,48 @@
+package br.com.marcioviana.gerenciadordemultas.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 25)
+    private String nome;
+    @Column(nullable = false, length = 25)
+    private String sobrenome;
+    @Column(nullable = false, length = 11)
+    @CPF
+    private String cpf;
+    @Column(nullable = false)
+    private Integer rg;
+    @Column(nullable = false, length = 200)
+    private String endereco;
+    @Column(nullable = false)
+    @Email
+    private String email;
+    @Column(nullable = false, length = 14)
+    private String telefone;
+
+    public Cliente(String nome, String sobrenome,
+                   String cpf, Integer rg, String endereco,
+                   String email, String telefone) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    /**
+     * Construtor de uso exclusivo do hibernate
+     */
+    @Deprecated
+    public Cliente() {
+    }
+}
